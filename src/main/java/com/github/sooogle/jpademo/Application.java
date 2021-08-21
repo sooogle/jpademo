d008466bd8dbbce4ec8e31834006c2ebf31fcf5d
sooogle <irishmanu4@gmail.com>
2021-08-22 01:36:31 +0900

    分離したパッケージをスキャン対象に追加

--- src/main/java/com/github/sooogle/jpademo/Application.java
+++ src/main/java/com/github/sooogle/jpademo/Application.java
@@ -1,12 +1,13 @@
 package com.github.sooogle.jpademo;
 
 import com.github.sooogle.jpademo.entity.Owner;
+import com.github.sooogle.jpademo.entitysub.PetEager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackageClasses = Owner.class)
+@EntityScan(basePackageClasses = {Owner.class, PetEager.class})
 public class Application {
 
 	public static void main(String[] args) {
